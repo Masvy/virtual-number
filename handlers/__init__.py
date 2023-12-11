cf629c5c6c40b7af484c24d7fab6a6343e00d10a
Masvy <masvy.435@gmail.com>
2023-12-11 21:29:00 +0300

    chore: import routers

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,11 @@
+from .information import information_router
+from .profile import profile_router
 from .start import start_router
 
 routers_list = [
     start_router,
+    information_router,
+    profile_router,
 ]
 
 __all__ = [
