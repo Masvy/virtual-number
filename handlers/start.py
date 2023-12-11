ad8644c2c20d2ccb6cd445c3fdbe5c937da6ff2c
Masvy <masvy.435@gmail.com>
2023-12-11 21:29:42 +0300

    feat: add callback processing

--- handlers/start.py
+++ handlers/start.py
@@ -1,6 +1,6 @@
 from aiogram.filters import CommandStart
+from aiogram import Router, F, types
 from aiogram.types import Message
-from aiogram import Router
 
 from keyboards.user_keboards import main_kb
 from lexicon.user_lexicon import USERS
@@ -9,6 +9,11 @@ start_router = Router()
 
 
 @start_router.message(CommandStart())
-async def start_bot(message: Message):
-    await message.answer(text=USERS['greetings'],
-                         reply_markup=main_kb)
+@start_router.callback_query(F.data == 'back_pressed')
+async def start_bot(update: types.Update):
+    if isinstance(update, types.Message):
+        await update.answer(text=USERS['greetings'],
+                            reply_markup=main_kb)
+    else:
+        await update.message.edit_text(USERS['greetings'])
+        await update.message.edit_reply_markup(reply_markup=main_kb)
