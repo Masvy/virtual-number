1fa7be5a3795cb4c6fb24d307256fc346daf2006
Masvy <masvy.435@gmail.com>
2023-12-11 21:25:20 +0300

    feat: create two keyboard objects

--- keyboards/user_keboards.py
+++ keyboards/user_keboards.py
@@ -10,9 +10,39 @@ main_kb: InlineKeyboardMarkup = InlineKeyboardMarkup(
             InlineKeyboardButton(text=KEYBOARDS['information'],
                                  callback_data='information_pressed'),
             InlineKeyboardButton(text=KEYBOARDS['profile'],
-                                 callback_data='profile_pressed')
+                                 callback_data='profile_pressed'),
         ],
         [InlineKeyboardButton(text=KEYBOARDS['referral'],
-                              callback_data='referral_pressed')]
+                              callback_data='referral_pressed')],
+    ]
+)
+
+information_kb: InlineKeyboardMarkup = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text=KEYBOARDS['rules'],
+                                 url='https://teletype.in/@toptgdima/e1DtyI3GrCJ'),
+            InlineKeyboardButton(text=KEYBOARDS['news'],
+                                 url='https://t.me/TopTgDima'),
+        ],
+        [InlineKeyboardButton(text=KEYBOARDS['admin'],
+                              url='https://t.me/TopTgCreator')],
+        [InlineKeyboardButton(text=KEYBOARDS['back'],
+                              callback_data='back_pressed')]
+    ]
+)
+
+profile_kb: InlineKeyboardMarkup = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+           InlineKeyboardButton(text=KEYBOARDS['replenish'],
+                                callback_data='replenish_pressed'),
+           InlineKeyboardButton(text=KEYBOARDS['status'],
+                                callback_data='status_pressed'),
+        ],
+        [InlineKeyboardButton(text=KEYBOARDS['bot_constructor'],
+                              callback_data='bot_constructor_pressed')],
+        [InlineKeyboardButton(text=KEYBOARDS['back'],
+                              callback_data='back_pressed')],
     ]
 )
