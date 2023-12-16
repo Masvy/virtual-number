5a8ecb3f4514b33108d297b02391a9d86eaf7d7d
Masvy <masvy.435@gmail.com>
2023-12-17 00:01:34 +0300

    chore: clear the state

--- handlers/start.py
+++ handlers/start.py
@@ -1,6 +1,6 @@
+from aiogram.fsm.context import FSMContext
 from aiogram.filters import CommandStart
 from aiogram import Router, F, types
-from aiogram.types import Message
 
 from keyboards.user_keboards import main_kb
 from lexicon.user_lexicon import USERS
@@ -10,10 +10,12 @@ start_router = Router()
 
 @start_router.message(CommandStart())
 @start_router.callback_query(F.data == 'back_pressed_1')
-async def start_bot(update: types.Update):
+async def start_bot(update: types.Update,
+                    state: FSMContext):
     if isinstance(update, types.Message):
         await update.answer(text=USERS['greetings'],
                             reply_markup=main_kb)
     else:
         await update.message.edit_text(USERS['greetings'])
         await update.message.edit_reply_markup(reply_markup=main_kb)
+    await state.clear()
