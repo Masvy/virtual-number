c915a1e378dd4cff41fbc985ca0de19af068bcfc
Masvy <masvy.435@gmail.com>
2023-12-16 23:49:15 +0300

    chore: reconfigure imports

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,8 +1,10 @@
 from .information import information_router
 from .profile import profile_router
+from .admin import admin_router
 from .start import start_router
 
 routers_list = [
+    admin_router,
     start_router,
     information_router,
     profile_router,
