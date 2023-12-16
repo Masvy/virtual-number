52641bdf08620af546a2f476da4ec3b3e7e7d7f4
Masvy <masvy.435@gmail.com>
2023-12-16 23:56:01 +0300

    feat: create several functions to manipulate the database

--- db/users.py
+++ db/users.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select
+from sqlalchemy import select, update
 
 from db.database import async_session
 from db.model import Users
@@ -9,3 +9,39 @@ async def read_users(user_id):
         result = await session.execute(select(Users).where(Users.user_id == user_id))
         user = result.all()
         return user
+
+
+async def update_account(user_id, amount):
+    async with async_session() as session:
+        await session.execute(update(Users).where(Users.user_id == user_id).values(account=Users.account+amount))
+        await session.execute(
+            update(Users).where(Users.user_id == user_id).values(completed=Users.completed + amount))
+        await session.commit()
+
+
+async def subtract_money(user_id, amount):
+    async with async_session() as session:
+        await session.execute(update(Users).where(Users.user_id == user_id).values(account=Users.account-amount))
+        await session.execute(
+            update(Users).where(Users.user_id == user_id).values(discarded=Users.discarded + amount))
+        if amount == 250:
+            await session.execute(update(Users).where(Users.user_id == user_id).values(status='На месяц'))
+        elif amount == 650:
+            await session.execute(update(Users).where(Users.user_id == user_id).values(status='На 3 месяца'))
+        else:
+            await session.execute(update(Users).where(Users.user_id == user_id).values(status='На 6 месяцев'))
+        await session.commit()
+
+
+async def read_account(user_id):
+    async with async_session() as session:
+        result = await session.execute(select(Users.account).where(Users.user_id == user_id))
+        account = result.scalar()
+        return account
+
+
+async def show_user_ids():
+    async with async_session() as session:
+        result = await session.execute(select(Users.user_id))
+        user_ids = [row[0] for row in result]
+        return user_ids
