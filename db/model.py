8b36cebba0f77e0e4baecbda48578a7a9a9a5dd2
Masvy <masvy.435@gmail.com>
2023-12-16 23:54:45 +0300

    feat: change the 'users' table

--- db/model.py
+++ db/model.py
@@ -14,8 +14,11 @@ class Users(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(BigInteger)
-    user_name: Mapped[str] = mapped_column(String(32))
+    user_name: Mapped[str] = mapped_column(String(32), nullable=True)
     registration_at: Mapped[datetime.datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
     status: Mapped[str] = mapped_column(String(12), server_default='Неактивен')
     partners: Mapped[int | None] = mapped_column(default=0)
     bots: Mapped[int | None] = mapped_column(default=0)
+    account: Mapped[int] = mapped_column(default=0)
+    completed: Mapped[int] = mapped_column(default=0)
+    discarded: Mapped[int] = mapped_column(default=0)
